86fc2274133a6e4ebd1be79660e03c8c2fb1281c
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-02-17 20:08:36 +0700

    feat: add StockChartScreen class

--- src/screens/stock_chart_screen.py
+++ src/screens/stock_chart_screen.py
@@ -1,2 +1,9 @@
 class StockChartScreen:
-    pass
+    def __init__(**kwargs):
+        super().__init__(**kwargs)
+
+    def load_stock_data(self):
+        pass
+
+    def update_chart(self):
+        pass
