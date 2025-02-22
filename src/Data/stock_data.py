e98d441d887eeeae9dc6f20a469908968439fd87
Worachat <worachat.smn@gmail.com>
2025-02-22 15:24:17 +0700

    add: handle 1 week

--- src/Data/stock_data.py
+++ src/Data/stock_data.py
@@ -1,12 +1,16 @@
 import yfinance as yf
 import pandas as pd
-from datetime import datetime
+from datetime import datetime,timedelta
 
 
 def fetch_stock_data(ticker, period, interval):
     end_date = datetime.now()
-    data = yf.download(ticker, period=period, interval=interval)
+    if period == '1wk': 
+        start_date = end_date - timedelta(days=7) 
+        data = yf.download(ticker, start=start_date, end=end_date, interval=interval) 
+    else:
+        data = yf.download(ticker, period=period, interval=interval)
     return data
 
-data = fetch_stock_data("NVDA","1d","30m")
+data = fetch_stock_data("NVDA","1wk","1h")
 print(data.head())  
