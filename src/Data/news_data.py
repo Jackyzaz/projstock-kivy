d329291a84b0debd70febcd81625207f79848468
Worachat <worachat.smn@gmail.com>
2025-02-22 17:01:26 +0700

    add: format news

--- src/Data/news_data.py
+++ src/Data/news_data.py
@@ -7,8 +7,25 @@ def fetch_stock_news(ticker):
 
     if not news_list:
         return []
+    
+    formatted_news = []
 
-    return news_list
+    for news in news_list:
+        content = news.get("content", {})
+        thumbnail = content.get("thumbnail", {})
+        title = content.get("title", {})
+        summary = content.get("summary", {})
+        pubDate = content.get("pubDate",{})
+        dt_obj = datetime.strptime(pubDate, "%Y-%m-%dT%H:%M:%SZ")
+        
+        formatted_news.append({
+        "thumbnail": thumbnail,
+        "title": title,
+        "summary": summary,
+        "pubDate": dt_obj.strftime("%d/%m/%Y %H:%M:%S")
+        })
+
+    return formatted_news
     
 news_data = fetch_stock_news("NVDA")
 for x in news_data:
