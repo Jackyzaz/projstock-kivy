f82fc8fb2ac7807c8f65d9d1358f0bfdc8c3b990
Worachat <worachat.smn@gmail.com>
2025-02-22 15:18:55 +0700

    fix: function base fetch

--- src/Data/stock_data.py
+++ src/Data/stock_data.py
@@ -1,19 +1,12 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime
 
-class StockData:
-    def __init__(self,stock_symbol):
-        self.stock_symbol = stock_symbol
-    def fetch_stock_data(self):
-        stock = yf.Ticker(self.stock_symbol)
-        data = stock.history(period="6mo")
-        return data
 
-    def get_stock_info(self):
-        stock = yf.Ticker(self.stock_symbol)
-        return stock.info
+def fetch_stock_data(ticker, period, interval):
+    end_date = datetime.now()
+    data = yf.download(ticker, period=period, interval=interval)
+    return data
 
-stock = StockData("NVDA")
-all_info= stock.get_stock_info()
-print("Stock Name: ",all_info["displayName"])
-print("Current Price:",all_info["currentPrice"],"USD" )
+data = fetch_stock_data("NVDA","1d","30m")
+print(data.head())  
