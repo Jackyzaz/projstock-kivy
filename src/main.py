56b9e094227cc39f3476c34b1a19001585014d41
Worachat <worachat.smn@gmail.com>
2025-02-23 18:15:49 +0700

    fix: clean code

--- src/main.py
+++ src/main.py
@@ -1,21 +1,20 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+
 from screens.home_screen import HomeScreen
 from screens.stock_chart_screen import StockChartScreen
-from kivy.core.window import Window
 
-Window.size = (1280, 720)
-
-
-class StockDashboardApp(App):
+class MainApp(App):
     def build(self):
-        Builder.load_file("stock_dashboard.kv")
+        Builder.load_file("screens/HomeScreen.kv")
+        Builder.load_file("screens/StockChartScreen.kv")
+
         sm = ScreenManager()
         sm.add_widget(HomeScreen(name="home"))
         sm.add_widget(StockChartScreen(name="stock_chart"))
+
         return sm
 
-
 if __name__ == "__main__":
-    StockDashboardApp().run()
+    MainApp().run()
