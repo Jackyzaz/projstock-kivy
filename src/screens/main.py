00bc179711b05e744d379806059eba4924270419
Worachat <worachat.smn@gmail.com>
2025-03-06 18:14:21 +0700

    add: call able main

--- src/screens/main.py
+++ src/screens/main.py
@@ -1,20 +1,28 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
-from home_screen import HomeScreen
 from news_screen import NewsScreen
+from home_screen import HomeScreen
+
 
 
 class MainApp(MDApp):
     def build(self):
-        # โหลดไฟล์ .kv ของแต่ละหน้า
-        Builder.load_file("screens/HomeScreen.kv")
-        Builder.load_file("screens/NewsScreen.kv")
+        # กำหนดธีมของแอป
+        self.theme_cls.primary_palette = "Blue"  # กำหนดสีหลักเป็นสีน้ำเงิน
+        self.theme_cls.theme_style = "Dark"  # กำหนดธีมเป็น Dark Mode
 
-        sm = MDScreenManager()
-        sm.add_widget(HomeScreen(name="home"))
-        sm.add_widget(NewsScreen(name="news"))
-        return sm
+        # สร้าง MDScreenManager
+        screen_manager = MDScreenManager()
 
-    def switch_screen(self, screen_name):
-        self.root.current = screen_name
+        # เพิ่มหน้าจอ NewsScreen และ HomeScreen
+        screen_manager.add_widget(NewsScreen(name="news"))
+        screen_manager.add_widget(HomeScreen(name="home"))
+
+        # ตั้งค่าให้แสดงหน้าจอ "news" เป็นค่าเริ่มต้น
+        screen_manager.current = "home"  
+
+        return screen_manager
+
+if __name__ == "__main__":
+    MainApp().run()
