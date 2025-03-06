e0e36cc03790f725c722409b0b54141ae9926e49
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-06 20:29:04 +0700

    Refactor: Migrate HomeScreen to ScreenManager in main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import Builder
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+from screens.home_screen import HomeScreen
+from screens.news_screen import NewsScreen
 
 
-class MainApp(App):
+class MainApp(MDApp):
     def build(self):
-        pass
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Dark"
+
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name="home_screen"))
+        return sm
 
 
 if __name__ == "__main__":
