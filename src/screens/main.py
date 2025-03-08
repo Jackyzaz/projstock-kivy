97b582c4ee0d68496a2dbb240bbeb4122ea4a24a
Worachat <worachat.smn@gmail.com>
2025-03-08 15:15:21 +0700

    add: main debug

--- src/screens/main.py
+++ src/screens/main.py
@@ -1,30 +1,39 @@
 from kivy.lang import Builder
-from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from news_screen import NewsScreen
 from home_screen import HomeScreen
+from search_screen import SearchScreen
 from kivymd.uix.navigationdrawer import MDNavigationDrawer
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.list import OneLineIconListItem
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivymd.tools.hotreload.app import MDApp
 
 Builder.load_file('NewsScreen.kv')
-Builder.load_file('HomeScreen.kv')
+# Builder.load_file('HomeScreen.kv')
+Builder.load_file('SearchScreen.kv')
+
 class MainApp(MDApp):
-    def build(self):
+    KV_FILES = ["./src/screens/SearchScreen.kv"]
+    DEBUG=True
+
+#Just build and delete first
+    def build_app(self,first=False):
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Dark"
 
         screen_manager = MDScreenManager()
 
+        screen_manager.add_widget(SearchScreen(name="search"))
         screen_manager.add_widget(NewsScreen(name="news"))
-        screen_manager.add_widget(HomeScreen(name="home"))
+        # screen_manager.add_widget(HomeScreen(name="home"))
+
 
         root = Builder.load_file("main.kv")
 
-        screen_manager.current = "home"
+        screen_manager.current = "search"
 
         return root
     
