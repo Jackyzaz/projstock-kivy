f03306a207dcbbb75ee398af866b2d6968099791
Soravit Sukkarn <soravit.sukkarn@gmail.com>
2025-03-09 22:26:41 +0700

    remove comment in main

--- src/screens/main.py
+++ src/screens/main.py
@@ -9,25 +9,13 @@ from kivy.core.window import Window
 
 
 class MainApp(MDApp):
-    # KV_FILES = ["./src/screens/SearchScreen.kv"]
-    # DEBUG=True
-
-    # Just build_app and delete first=False
     def build(self):
         Window.size = (1280, 720)
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Dark"
 
         self.root.ids.screen_manager.current = "home"
-        # screen_manager = MDScreenManager()
 
-        # screen_manager.add_widget(HomeScreen(name="home"))
-        # screen_manager.add_widget(NewsScreen(name="news"))
-        # screen_manager.add_widget(SearchScreen(name="search"))
-        # screen_manager.add_widget(SearchScreen(name="search"))
-        # screen_manager.add_widget(SearchScreen(name="search"))
-
-        # screen_manager.current = "home"
         root = Builder.load_file("main.kv")
 
         return root
